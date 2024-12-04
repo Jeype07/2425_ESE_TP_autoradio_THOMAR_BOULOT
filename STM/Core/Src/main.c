d478b1f0cdc08619d77ec745a37d8527d278aa89
PaulTravail <paul.boulot@ensea.fr>
2024-12-04 15:49:01 +0100

    struct

--- STM/Core/Src/main.c
+++ STM/Core/Src/main.c
@@ -119,8 +119,6 @@ int addition(h_shell_t * h_shell, int argc, char ** argv)
 	}
 }
 
-
-
 int ledToggle(h_shell_t * h_shell, int argc, char ** argv)
 {
 	if (argc == 3)
@@ -154,10 +152,10 @@ int ledToggle(h_shell_t * h_shell, int argc, char ** argv)
 	}
 }
 
-void task_chenillard(void * unused) {
+void task_chenillard(void * params) {
+	h_shell_t *h_shell = (h_shell_t *)params;
 	uint8_t current_led_a = 0;
 	uint8_t current_led_b = 4;
-
 	while (1) {
 		if (chenillard_running) {
 			h_shell->drv.led(h_shell->led_num = current_led_a,0x12);
@@ -175,6 +173,10 @@ void task_chenillard(void * unused) {
 int chenillard(h_shell_t * h_shell, int argc, char ** argv){
 	if(chenillard_running == 0){
 		chenillard_running = 1; // Activer le chenillard
+		if (ChenillardTaskHandle == NULL) {
+			// Créer la tâche si elle n'existe pas
+			xTaskCreate(task_chenillard, "ChenillardTask", 128, NULL, 1, &ChenillardTaskHandle);
+		}
 	}
 	else{
 		chenillard_running = 0;
@@ -184,231 +186,229 @@ int chenillard(h_shell_t * h_shell, int argc, char ** argv){
 			MCP23S17_Write(MCPGPIOA, 0xff);
 			MCP23S17_Write(MCPGPIOB, 0xff);
 		}
-		if (ChenillardTaskHandle == NULL) {
-			// Créer la tâche si elle n'existe pas
-			xTaskCreate(task_chenillard, "ChenillardTask", 128, NULL, 1, &ChenillardTaskHandle);
-		}
-		return 0;
-
 	}
 
 
-	void task_shell(void * unused)
+	return 0;
+}
+
+
+void task_shell(void * unused)
+{
+	shell_init(&h_shell);
+	shell_add(&h_shell, 'f', fonction, "Une fonction inutile");
+	shell_add(&h_shell, 'a', addition, "Effectue une somme");
+	shell_add(&h_shell, 'b', ledToggle, "Allumer une led");
+	shell_add(&h_shell, 'c',chenillard, "Lancer chenillard/Arreter chenillard");
+	shell_run(&h_shell);	// boucle infinie
+}
+
+
+/* USER CODE END 0 */
+
+/**
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void)
+{
+
+	/* USER CODE BEGIN 1 */
+
+	/* USER CODE END 1 */
+
+	/* MCU Configuration--------------------------------------------------------*/
+
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
+
+	/* USER CODE BEGIN Init */
+
+	/* USER CODE END Init */
+
+	/* Configure the system clock */
+	SystemClock_Config();
+
+	/* Configure the peripherals common clocks */
+	PeriphCommonClock_Config();
+
+	/* USER CODE BEGIN SysInit */
+
+	/* USER CODE END SysInit */
+
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_DMA_Init();
+	MX_USART2_UART_Init();
+	MX_SPI3_Init();
+	MX_I2C2_Init();
+	MX_SAI2_Init();
+	/* USER CODE BEGIN 2 */
+
+	__HAL_SAI_ENABLE(&hsai_BlockA2);
+
+	h_shell.drv.receive = drv_uart2_receive;
+	h_shell.drv.transmit = drv_uart2_transmit;
+	h_shell.drv.init = MCP23S17_Init;
+	h_shell.drv.led = drv_led;
+
+
+	if (xTaskCreate(task_shell, "Shell", TASK_SHELL_STACK_DEPTH, NULL, TASK_SHELL_PRIORITY, &h_task_shell) != pdPASS)
 	{
-		shell_init(&h_shell);
-		shell_add(&h_shell, 'f', fonction, "Une fonction inutile");
-		shell_add(&h_shell, 'a', addition, "Effectue une somme");
-		shell_add(&h_shell, 'b', ledToggle, "Allumer une led");
-		shell_add(&h_shell, 'c',chenillard, "Lancer chenillard/Arreter chenillard");
-		shell_run(&h_shell);	// boucle infinie
+		printf("Error creating task shell\r\n");
+		Error_Handler();
 	}
 
+	vTaskStartScheduler();
+	/* USER CODE END 2 */
 
-	/* USER CODE END 0 */
+	/* Call init function for freertos objects (in cmsis_os2.c) */
+	MX_FREERTOS_Init();
 
-	/**
-	 * @brief  The application entry point.
-	 * @retval int
+	/* Start scheduler */
+	osKernelStart();
+
+	/* We should never get here as control is now taken by the scheduler */
+
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1)
+	{
+
+		/* USER CODE END WHILE */
+
+		/* USER CODE BEGIN 3 */
+	}
+	/* USER CODE END 3 */
+}
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void)
+{
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+	/** Configure the main internal regulator output voltage
 	 */
-	int main(void)
+	if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
 	{
-
-		/* USER CODE BEGIN 1 */
-
-		/* USER CODE END 1 */
-
-		/* MCU Configuration--------------------------------------------------------*/
-
-		/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-		HAL_Init();
-
-		/* USER CODE BEGIN Init */
-
-		/* USER CODE END Init */
-
-		/* Configure the system clock */
-		SystemClock_Config();
-
-		/* Configure the peripherals common clocks */
-		PeriphCommonClock_Config();
-
-		/* USER CODE BEGIN SysInit */
-
-		/* USER CODE END SysInit */
-
-		/* Initialize all configured peripherals */
-		MX_GPIO_Init();
-		MX_DMA_Init();
-		MX_USART2_UART_Init();
-		MX_SPI3_Init();
-		MX_I2C2_Init();
-		MX_SAI2_Init();
-		/* USER CODE BEGIN 2 */
-
-		__HAL_SAI_ENABLE(&hsai_BlockA2);
-
-		h_shell.drv.receive = drv_uart2_receive;
-		h_shell.drv.transmit = drv_uart2_transmit;
-		h_shell.drv.init = MCP23S17_Init;
-		h_shell.drv.led = drv_led;
-
-
-		if (xTaskCreate(task_shell, "Shell", TASK_SHELL_STACK_DEPTH, NULL, TASK_SHELL_PRIORITY, &h_task_shell) != pdPASS)
-		{
-			printf("Error creating task shell\r\n");
-			Error_Handler();
-		}
-
-		vTaskStartScheduler();
-		/* USER CODE END 2 */
-
-		/* Call init function for freertos objects (in cmsis_os2.c) */
-		MX_FREERTOS_Init();
-
-		/* Start scheduler */
-		osKernelStart();
-
-		/* We should never get here as control is now taken by the scheduler */
-
-		/* Infinite loop */
-		/* USER CODE BEGIN WHILE */
-		while (1)
-		{
-
-			/* USER CODE END WHILE */
-
-			/* USER CODE BEGIN 3 */
-		}
-		/* USER CODE END 3 */
+		Error_Handler();
 	}
 
-	/**
-	 * @brief System Clock Configuration
-	 * @retval None
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
 	 */
-	void SystemClock_Config(void)
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLM = 1;
+	RCC_OscInitStruct.PLL.PLLN = 10;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+	RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+	RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
 	{
-		RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-		RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-		/** Configure the main internal regulator output voltage
-		 */
-		if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
-		{
-			Error_Handler();
-		}
-
-		/** Initializes the RCC Oscillators according to the specified parameters
-		 * in the RCC_OscInitTypeDef structure.
-		 */
-		RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-		RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-		RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-		RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-		RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-		RCC_OscInitStruct.PLL.PLLM = 1;
-		RCC_OscInitStruct.PLL.PLLN = 10;
-		RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
-		RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-		RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-		if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-		{
-			Error_Handler();
-		}
-
-		/** Initializes the CPU, AHB and APB buses clocks
-		 */
-		RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-				|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-		RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-		RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-		RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-		RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-		if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-		{
-			Error_Handler();
-		}
+		Error_Handler();
 	}
 
-	/**
-	 * @brief Peripherals Common Clock Configuration
-	 * @retval None
+	/** Initializes the CPU, AHB and APB buses clocks
 	 */
-	void PeriphCommonClock_Config(void)
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
 	{
-		RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
-
-		/** Initializes the peripherals clock
-		 */
-		PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_SAI2;
-		PeriphClkInit.Sai2ClockSelection = RCC_SAI2CLKSOURCE_PLLSAI1;
-		PeriphClkInit.PLLSAI1.PLLSAI1Source = RCC_PLLSOURCE_HSI;
-		PeriphClkInit.PLLSAI1.PLLSAI1M = 1;
-		PeriphClkInit.PLLSAI1.PLLSAI1N = 13;
-		PeriphClkInit.PLLSAI1.PLLSAI1P = RCC_PLLP_DIV17;
-		PeriphClkInit.PLLSAI1.PLLSAI1Q = RCC_PLLQ_DIV2;
-		PeriphClkInit.PLLSAI1.PLLSAI1R = RCC_PLLR_DIV2;
-		PeriphClkInit.PLLSAI1.PLLSAI1ClockOut = RCC_PLLSAI1_SAI1CLK;
-		if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-		{
-			Error_Handler();
-		}
+		Error_Handler();
 	}
+}
 
-	/* USER CODE BEGIN 4 */
+/**
+ * @brief Peripherals Common Clock Configuration
+ * @retval None
+ */
+void PeriphCommonClock_Config(void)
+{
+	RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-	/* USER CODE END 4 */
-
-	/**
-	 * @brief  Period elapsed callback in non blocking mode
-	 * @note   This function is called  when TIM1 interrupt took place, inside
-	 * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
-	 * a global variable "uwTick" used as application time base.
-	 * @param  htim : TIM handle
-	 * @retval None
+	/** Initializes the peripherals clock
 	 */
-	void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_SAI2;
+	PeriphClkInit.Sai2ClockSelection = RCC_SAI2CLKSOURCE_PLLSAI1;
+	PeriphClkInit.PLLSAI1.PLLSAI1Source = RCC_PLLSOURCE_HSI;
+	PeriphClkInit.PLLSAI1.PLLSAI1M = 1;
+	PeriphClkInit.PLLSAI1.PLLSAI1N = 13;
+	PeriphClkInit.PLLSAI1.PLLSAI1P = RCC_PLLP_DIV17;
+	PeriphClkInit.PLLSAI1.PLLSAI1Q = RCC_PLLQ_DIV2;
+	PeriphClkInit.PLLSAI1.PLLSAI1R = RCC_PLLR_DIV2;
+	PeriphClkInit.PLLSAI1.PLLSAI1ClockOut = RCC_PLLSAI1_SAI1CLK;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
 	{
-		/* USER CODE BEGIN Callback 0 */
-
-		/* USER CODE END Callback 0 */
-		if (htim->Instance == TIM1) {
-			HAL_IncTick();
-		}
-		/* USER CODE BEGIN Callback 1 */
-
-		/* USER CODE END Callback 1 */
+		Error_Handler();
 	}
+}
 
-	/**
-	 * @brief  This function is executed in case of error occurrence.
-	 * @retval None
-	 */
-	void Error_Handler(void)
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+ * @brief  Period elapsed callback in non blocking mode
+ * @note   This function is called  when TIM1 interrupt took place, inside
+ * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+ * a global variable "uwTick" used as application time base.
+ * @param  htim : TIM handle
+ * @retval None
+ */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	/* USER CODE BEGIN Callback 0 */
+
+	/* USER CODE END Callback 0 */
+	if (htim->Instance == TIM1) {
+		HAL_IncTick();
+	}
+	/* USER CODE BEGIN Callback 1 */
+
+	/* USER CODE END Callback 1 */
+}
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void)
+{
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1)
 	{
-		/* USER CODE BEGIN Error_Handler_Debug */
-		/* User can add his own implementation to report the HAL error return state */
-		__disable_irq();
-		while (1)
-		{
-		}
-		/* USER CODE END Error_Handler_Debug */
 	}
+	/* USER CODE END Error_Handler_Debug */
+}
 
 #ifdef  USE_FULL_ASSERT
-	/**
-	 * @brief  Reports the name of the source file and the source line number
-	 *         where the assert_param error has occurred.
-	 * @param  file: pointer to the source file name
-	 * @param  line: assert_param error line source number
-	 * @retval None
-	 */
-	void assert_failed(uint8_t *file, uint32_t line)
-	{
-		/* USER CODE BEGIN 6 */
-		/* User can add his own implementation to report the file name and line number,
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+	/* USER CODE BEGIN 6 */
+	/* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-		/* USER CODE END 6 */
-	}
+	/* USER CODE END 6 */
+}
 #endif /* USE_FULL_ASSERT */
